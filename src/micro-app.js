18d09eda7bf208b196e4a3c587040d74bf758259
zoucw <326359613@qq.com>
2021-11-28 11:29:35 +0800

    子服务更改

--- src/micro-app.js
+++ src/micro-app.js
@@ -4,12 +4,12 @@
  * @Author: chunwen
  * @Date: 2021-11-01 17:24:48
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-11-17 11:13:16
+ * @LastEditTime: 2021-11-28 11:28:39
  */
 const microApps = [
   {
     name: 'micro_vue',
-    entry: '//localhost:8081/micro_vue',
+    entry: '//localhost:4001/micro_vue',
     activeRule: '/micro_vue',
     container: '#subapp-viewport', // 子应用挂载的div
     props: {
@@ -18,7 +18,7 @@ const microApps = [
   },
   {
     name: 'micro_react',
-    entry: '//localhost:8888/micro_react',
+    entry: '//localhost:4002/micro_react',
     activeRule: '/micro_react',
     container: '#subapp-viewport', // 子应用挂载的div
     props: {
