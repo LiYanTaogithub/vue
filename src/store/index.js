c0a450dc09b3b4da773c8179f46c65b273a2bfae
zoucw <326359613@qq.com>
2022-03-29 17:12:40 +0800

    store增加menulist

--- src/store/index.js
+++ src/store/index.js
@@ -4,19 +4,21 @@
  * @Author: chunwen
  * @Date: 2021-11-09 22:36:47
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-11-12 18:35:43
+ * @LastEditTime: 2022-03-29 16:01:01
  */
 import { createStore } from 'vuex'
 
 import app from './modules/app'
 import user from './modules/user'
 import keepAlive from './modules/keepAlive'
+import menuList from './modules/menuList'
 
 export default createStore({
   modules: {
     app,
     user,
     keepAlive,
+    menuList,
   },
   strict: true,
 })
