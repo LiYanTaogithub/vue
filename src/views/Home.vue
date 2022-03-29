cd46af9d451796f92a9bc37d0834f4a89978f38c
zoucw <326359613@qq.com>
2022-03-29 17:14:34 +0800

    调试代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,22 +4,33 @@
  * @Author: chunwen
  * @Date: 2021-11-09 22:36:47
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-11-11 17:31:53
+ * @LastEditTime: 2022-03-29 15:04:53
 -->
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <el-button type="primary" @click="hanleClick">{{$store.state.user.info.age}}</el-button>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import { useStore } from 'vuex'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+  },
+  setup() {
+    const store = useStore()
+    const hanleClick = () => {
+      store.commit('user/addAge')
+    }
+    return {
+      hanleClick,
+    }
   },
 }
 </script>
